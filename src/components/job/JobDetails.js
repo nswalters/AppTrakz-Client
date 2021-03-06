6a590e5bc19cb8fdfece661305726ada7261b702
Nick Walters <nswalters.s@gmail.com>
2021-03-06 15:28:23 -0600

    Cleaned up boiler-plate code

--- src/components/job/JobDetails.js
+++ src/components/job/JobDetails.js
@@ -38,17 +38,8 @@ export const JobDetails = (props) => {
           <div className="flex items-center space-x-5">
             <div>
               <h1 className="text-2xl font-bold text-gray-900">{singleJob.role_title}</h1>
-              {/* <p className="text-sm font-medium text-gray-500">Applied for <a href="/" className="text-gray-900">Front End Developer</a> on <time dateTime="2020-08-25">August 25, 2020</time></p> */}
             </div>
           </div>
-          {/* <div className="mt-6 flex flex-col-reverse justify-stretch space-y-4 space-y-reverse sm:flex-row-reverse sm:justify-end sm:space-x-reverse sm:space-y-0 sm:space-x-3 md:mt-0 md:flex-row md:space-x-3">
-            <button type="button" className="inline-flex items-center justify-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-blue-500">
-              Disqualify
-            </button>
-            <button type="button" className="inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-blue-500">
-              Advance to offer
-            </button>
-          </div> */}
         </div>
 
         <div className="mt-8 max-w-3xl mx-auto grid grid-cols-1 gap-6 sm:px-6 lg:max-w-7xl lg:grid-flow-col-dense lg:grid-cols-3">
@@ -122,56 +113,12 @@ export const JobDetails = (props) => {
                         {singleJob.description}
                       </dd>
                     </div>
-                    {/* <div className="sm:col-span-2">
-                      <dt className="text-sm font-medium text-gray-500">
-                        Attachments
-                      </dt>
-                      <dd className="mt-1 text-sm text-gray-900">
-                        <ul className="border border-gray-200 rounded-md divide-y divide-gray-200">
-                          <li className="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
-                            <div className="w-0 flex-1 flex items-center">
-                              <!-- Heroicon name: solid/paper-clip -->
-                              <svg className="flex-shrink-0 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                <path fillRule="evenodd" d="M8 4a3 3 0 00-3 3v4a5 5 0 0010 0V7a1 1 0 112 0v4a7 7 0 11-14 0V7a5 5 0 0110 0v4a3 3 0 11-6 0V7a1 1 0 012 0v4a1 1 0 102 0V7a3 3 0 00-3-3z" clipRule="evenodd" />
-                              </svg>
-                              <span className="ml-2 flex-1 w-0 truncate">
-                                resume_back_end_developer.pdf
-                              </span>
-                            </div>
-                            <div className="ml-4 flex-shrink-0">
-                              <a href="/" className="font-medium text-blue-600 hover:text-blue-500">
-                                Download
-                              </a>
-                            </div>
-                          </li>
-                          <li className="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
-                            <div className="w-0 flex-1 flex items-center">
-                              <!-- Heroicon name: solid/paper-clip -->
-                              <svg className="flex-shrink-0 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                <path fillRule="evenodd" d="M8 4a3 3 0 00-3 3v4a5 5 0 0010 0V7a1 1 0 112 0v4a7 7 0 11-14 0V7a5 5 0 0110 0v4a3 3 0 11-6 0V7a1 1 0 012 0v4a1 1 0 102 0V7a3 3 0 00-3-3z" clipRule="evenodd" />
-                              </svg>
-                              <span className="ml-2 flex-1 w-0 truncate">
-                                coverletter_back_end_developer.pdf
-                              </span>
-                            </div>
-                            <div className="ml-4 flex-shrink-0">
-                              <a href="/" className="font-medium text-blue-600 hover:text-blue-500">
-                                Download
-                              </a>
-                            </div>
-                          </li>
-                        </ul>
-                      </dd>
-                    </div> */}
                   </dl>
                 </div>
-                {/* <div>
-                  <a href="/" className="block bg-gray-50 text-sm font-medium text-gray-500 text-center px-4 py-4 hover:text-gray-700 sm:rounded-b-lg">Read full application</a>
-                </div> */}
               </div>
             </section>
 
-            {/* <!-- Comments--> */}
+            {/* <!-- Notes --> */}
             {/* <section aria-labelledby="notes-title">
               <div className="bg-white shadow sm:rounded-lg sm:overflow-hidden">
                 <div className="divide-y divide-gray-200">
@@ -305,7 +252,6 @@ export const JobDetails = (props) => {
                             <p className="text-sm text-gray-500">Applied to <a href={jobApplication.job && jobApplication.job.url} className="font-medium text-gray-900">{jobApplication.job && jobApplication.job.role_title}</a></p>
                           </div>
                           <div className="text-right text-sm whitespace-nowrap text-gray-500">
-                            {/* <time dateTime="2020-09-20">{jobApplication.submitted_at}</time> */}
                             <time dateTime={jobApplication.submitted_at && (jobApplication.submitted_at).split('T')[0]}>{new Date(jobApplication.submitted_at).toLocaleString('en-us', { month: 'short', day: 'numeric' })}</time>
                           </div>
                         </div>
@@ -323,9 +269,8 @@ export const JobDetails = (props) => {
                     return (
                       <li>
                         <div className="relative pb-8">
-                          {/* Don't show the connector line if the status
-                            is the last one
-                        */}
+                          { /* Don't show the connector line if the status
+                            is the last one */}
                           {idx === arr.length - 1 ? '' : (
                             <span className="absolute top-3 left-3 -ml-px h-full w-0.5 bg-gray-200" aria-hidden="true"></span>)}
                           <div className="relative flex space-x-3">
@@ -346,103 +291,8 @@ export const JobDetails = (props) => {
                       </li>
                     );
                   })}
-
-                  {/* <li>
-                    <div className="relative pb-8">
-                      <div className="relative flex space-x-3">
-                        <div>
-                          <span className="h-6 w-6 rounded-full bg-gray-400 flex items-center justify-center ring-8 ring-white">
-                          </span>
-                        </div>
-                        <div className="min-w-0 flex-1 pt-1.5 flex justify-between space-x-4">
-                          <div>
-                            <p className="text-sm text-gray-500">{jobApplication.statuses && jobApplication.statuses[0].name}</p>
-                          </div>
-                          <div className="text-right text-sm whitespace-nowrap text-gray-500">
-                            <time dateTime="2020-09-22">Sep 22</time>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </li> */}
-
-                  {/* <li>
-                    <div className="relative pb-8">
-                      <span className="absolute top-4 left-4 -ml-px h-full w-0.5 bg-gray-200" aria-hidden="true"></span>
-                      <div className="relative flex space-x-3">
-                        <div>
-                          <span className="h-8 w-8 rounded-full bg-green-500 flex items-center justify-center ring-8 ring-white">
-                            <!-- Heroicon name: solid/check -->
-                            <svg className="h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                              <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
-                            </svg>
-                          </span>
-                        </div>
-                        <div className="min-w-0 flex-1 pt-1.5 flex justify-between space-x-4">
-                          <div>
-                            <p className="text-sm text-gray-500">Completed phone screening with <a href="/" className="font-medium text-gray-900">Martha Gardner</a></p>
-                          </div>
-                          <div className="text-right text-sm whitespace-nowrap text-gray-500">
-                            <time dateTime="2020-09-28">Sep 28</time>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </li> */}
-
-                  {/* <li>
-                    <div className="relative pb-8">
-                      <span className="absolute top-4 left-4 -ml-px h-full w-0.5 bg-gray-200" aria-hidden="true"></span>
-                      <div className="relative flex space-x-3">
-                        <div>
-                          <span className="h-8 w-8 rounded-full bg-blue-500 flex items-center justify-center ring-8 ring-white">
-                            <!-- Heroicon name: solid/thumb-up -->
-                            <svg className="h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                              <path d="M2 10.5a1.5 1.5 0 113 0v6a1.5 1.5 0 01-3 0v-6zM6 10.333v5.43a2 2 0 001.106 1.79l.05.025A4 4 0 008.943 18h5.416a2 2 0 001.962-1.608l1.2-6A2 2 0 0015.56 8H12V4a2 2 0 00-2-2 1 1 0 00-1 1v.667a4 4 0 01-.8 2.4L6.8 7.933a4 4 0 00-.8 2.4z" />
-                            </svg>
-                          </span>
-                        </div>
-                        <div className="min-w-0 flex-1 pt-1.5 flex justify-between space-x-4">
-                          <div>
-                            <p className="text-sm text-gray-500">Advanced to interview by <a href="/" className="font-medium text-gray-900">Bethany Blake</a></p>
-                          </div>
-                          <div className="text-right text-sm whitespace-nowrap text-gray-500">
-                            <time dateTime="2020-09-30">Sep 30</time>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </li> */}
-
-                  {/* <li>
-                    <div className="relative pb-8">
-                      <div className="relative flex space-x-3">
-                        <div>
-                          <span className="h-8 w-8 rounded-full bg-green-500 flex items-center justify-center ring-8 ring-white">
-                            <!-- Heroicon name: solid/check -->
-                            <svg className="h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                              <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
-                            </svg>
-                          </span>
-                        </div>
-                        <div className="min-w-0 flex-1 pt-1.5 flex justify-between space-x-4">
-                          <div>
-                            <p className="text-sm text-gray-500">Completed interview with <a href="/" className="font-medium text-gray-900">Katherine Snyder</a></p>
-                          </div>
-                          <div className="text-right text-sm whitespace-nowrap text-gray-500">
-                            <time dateTime="2020-10-04">Oct 4</time>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </li> */}
                 </ul>
               </div>
-              {/* <div className="mt-6 flex flex-col justify-stretch">
-                <button type="button" className="inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-                  Advance to offer
-                </button>
-              </div> */}
             </div>
           </section>
         </div>
