5f7e3fa1aad080a3c38f1af6c5ab0b33291feab3
Nick Walters <nswalters.s@gmail.com>
2021-03-09 18:36:25 -0600

    Creating an initial application record works

--- src/components/application/ApplicationProvider.js
+++ src/components/application/ApplicationProvider.js
@@ -6,18 +6,31 @@ export const ApplicationProvider = (props) => {
   const [applicationList, setApplicationList] = useState([]);
 
   // Get applications
-  const getApplications = () => {
+  const getApplications = () => (
     fetch('http://localhost:8000/applications', {
       headers: {
         Authorization: `Token ${localStorage.getItem('apptrakz_token')}`,
       },
     })
       .then((res) => res.json())
-      .then(setApplicationList);
-  };
+      .then(setApplicationList)
+  );
+
+  // Create application
+  const createApplication = (applicationDetails) => (
+    fetch('http://localhost:8000/applications', {
+      method: 'POST',
+      headers: {
+        Authorization: `Token ${localStorage.getItem('apptrakz_token')}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(applicationDetails),
+    })
+      .then(getApplications())
+  );
 
   return (
-    <ApplicationContext.Provider value={{ applicationList, getApplications }}>
+    <ApplicationContext.Provider value={{ applicationList, createApplication, getApplications }}>
       { props.children}
     </ApplicationContext.Provider>
   );
