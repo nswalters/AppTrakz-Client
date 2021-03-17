acc406cfaaa44db8d9f63797f1b05236865df57a
Nick Walters <nswalters.s@gmail.com>
2021-03-17 04:30:41 -0500

    Fixed jobs applied for count

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -24,8 +24,10 @@ export const Dashboard = (props) => {
 
   useEffect(() => {
     /* Updated everytime 'jobList' state value is updated */
+    const jobsAppliedFor = jobList.filter((job) => job.application);
 
-    setTotalJobs(jobList.length);
+    // setTotalJobs(jobList.length);
+    setTotalJobs(jobsAppliedFor.length);
   }, [jobList]);
 
   useEffect(() => {
@@ -74,7 +76,7 @@ export const Dashboard = (props) => {
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
               </svg>
             </div>
-            <p className="ml-16 text-sm font-medium text-gray-500 truncate">Total Jobs Saved</p>
+            <p className="ml-16 text-sm font-medium text-gray-500 truncate">Total Jobs Applied For</p>
           </dt>
           <dd className="ml-16 pb-6 flex items-baseline sm:pb-7">
             <p className="text-2xl font-semibold text-gray-900">
