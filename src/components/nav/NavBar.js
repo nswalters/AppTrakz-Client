2e90109635293e1b95f2d987268162bbab9ba11d
Nick Walters <nswalters.s@gmail.com>
2021-03-20 18:46:52 -0500

    modified tag

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -169,10 +169,10 @@ export const NavBar = (props) => {
             </div>
             <div className="mt-3 space-y-1">
               <Link to="/user/profile" className="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100 sm:px-6">Your Profile</Link>
-              <a onClick={() => {
+              <p onClick={() => {
                 localStorage.removeItem('apptrakz_token');
                 props.history.push({ pathname: '/' });
-              }} href="/" className="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100 sm:px-6">Sign out</a>
+              }} href="/" className="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100 sm:px-6">Sign out</p>
             </div>
           </div>
         </div>
