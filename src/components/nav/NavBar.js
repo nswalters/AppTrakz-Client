7379241e13a97d83eb26343c38c46499058c189a
Nick Walters <nswalters.s@gmail.com>
2021-03-20 18:46:09 -0500

    Fix 'Your Profile' option in collapsed menu

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -168,7 +168,7 @@ export const NavBar = (props) => {
               </div>
             </div>
             <div className="mt-3 space-y-1">
-              <a href="/" className="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100 sm:px-6">Your Profile</a>
+              <Link to="/user/profile" className="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100 sm:px-6">Your Profile</Link>
               <a onClick={() => {
                 localStorage.removeItem('apptrakz_token');
                 props.history.push({ pathname: '/' });
