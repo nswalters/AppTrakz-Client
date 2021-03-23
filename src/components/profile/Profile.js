d365e5c6c9b15aeffee2de713337da376e6cac0f
Nick Walters <nswalters.s@gmail.com>
2021-03-22 19:30:07 -0500

    Updated redirect when saving profile

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -184,7 +184,7 @@ export const Profile = (props) => {
         <button onClick={() => history.goBack()} type="button" className="bg-white border border-gray-300 rounded-md shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
           Cancel
         </button>
-        <button onClick={() => { updateProfile(currentUserProfile); history.push('/'); }} type="submit" className="ml-5 bg-indigo-700 border border-transparent rounded-md shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium text-white hover:bg-indigo-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+        <button onClick={() => { updateProfile(currentUserProfile); history.push('/dashboard'); }} type="submit" className="ml-5 bg-indigo-700 border border-transparent rounded-md shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium text-white hover:bg-indigo-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
           Save
         </button>
       </div>
